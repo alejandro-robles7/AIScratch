ac359a54ba6715398d251640408d85fd3ddd886d
Alejandro Robles <alejandro.robles@altruisconsulting.com>
2019-03-06 22:26:59 -0800

    Adding playing tennis example for bayesian calculation

---
+++ scripts/bayes.py
@@ -0,0 +1,48 @@
+from pandas import read_csv
+
+df = read_csv(r'data/playtennis.csv')
+
+
+def calc(df, colname, attribute, target=1, targetcol= 'Play'):
+    df = df[df[targetcol] == target]
+    n = len(df)
+    return sum(df[colname] == attribute) * 1.0 / n
+
+
+
+dat = dict()
+
+prob_yes = sum(df.Play == 1) * 1.0 /len(df)
+prob_no = sum(df.Play == 0) * 1.0 /len(df)
+prob = {'Yes': prob_yes, 'No': prob_no}
+
+
+
+for k, v in df.drop(['Play','Day'], axis=1).iteritems():
+    for attribute in v.unique():
+        yes = calc(df, k, attribute)
+        no = calc(df, k, attribute, 0)
+        print "Prob(" + k + ' = ' + attribute + ' | ' + 'Play = Yes) = ' + str(yes)
+        print "Prob(" + k + ' = ' + attribute + ' | ' + 'Play = No) = ' + str(no)
+        print '-----------------------------------------------------------------'
+        dat[attribute] = {'Yes': yes, 'No': no}
+
+
+def calc_prob(outlook, temperature, humidity, wind):
+    target = 'No'
+    prob_of_no = dat[outlook][target] * dat[temperature][target] * dat[humidity][target] * dat[wind][target] * prob[target]
+    target = 'Yes'
+    prob_of_yes = dat[outlook][target] * dat[temperature][target] * dat[humidity][target] * dat[wind][target] * prob[target]
+    if prob_of_yes >= prob_of_no:
+        print 'Predict yes'
+    else:
+        print 'Pridict no'
+
+    return {"Yes": prob_of_yes, "No": prob_of_no}
+
+
+outlook = 'Sunny'
+temperature = 'Cool'
+humidity = 'High'
+wind = 'Strong'
+prob1 = calc_prob(outlook, temperature, humidity, wind)
--- scripts/entropy.py
+++ scripts/entropy.py
@@ -1,19 +1,20 @@
 from math import log
 import numpy as np
 
-def ent(p):
-	if p == 0 or p==1:
-		return 0
-	else:
-		p2 = 1 - p
-		return -p*log(p, 2) - p2*log(p2, 2)
 
-def ent2(n, start_num, a, b):
-	start = start_num / n
-	aa, pp = a / n, b / a
-	pp2 = np.array([ent(p) for p in pp])
-	return ent(start) - aa.dot(pp2)
+def entropy(p):
+    if p == 0 or p == 1:
+        return 0
+    else:
+        p2 = 1 - p
+        return -p * log(p, 2) - p2 * log(p2, 2)
 
+
+def gain(n, start_num, a, b):
+    start = start_num / n
+    aa, pp = a / n, b / a
+    pp2 = np.array([entropy(p) for p in pp])
+    return entropy(start) - aa.dot(pp2)
 
 
 # For all
@@ -23,22 +24,69 @@ start_num = 9.0
 # Wind
 a = np.array([8.0, 6.0])
 b = np.array([6.0, 3.0])
-print 'wind', ent2(n, start_num, a,b)
-
+print 'wind', gain(n, start_num, a, b)
 
 # Humidity
 a = np.array([7.0, 7.0])
 b = np.array([3.0, 6.0])
-print 'humidity', ent2(n, start_num, a,b)
-
+print 'humidity', gain(n, start_num, a, b)
 
 # Temp
 a = np.array([4.0, 6.0, 4.0])
 b = np.array([2.0, 4.0, 3.0])
-print 'temp', ent2(n, start_num, a,b)
-
+print 'temp', gain(n, start_num, a, b)
 
 # Outlook
 a = np.array([5.0, 4.0, 5.0])
 b = np.array([2.0, 4.0, 3.0])
-print 'outlook', ent2(n, start_num, a,b)
+print 'outlook', gain(n, start_num, a, b)
+
+
+
+
+# Outlook is sunny
+n = 5.0
+start_num = 2.0
+
+# humidity
+a = np.array([3.0, 2.0])
+b = np.array([0.0, 2.0])
+print 'humidity', gain(n, start_num, a, b)
+
+
+# temp
+a = np.array([2.0, 2.0, 1.0])
+b = np.array([0.0, 1.0, 1.0])
+print 'temp', gain(n, start_num, a, b)
+
+
+# wind
+a = np.array([2.0, 2.0, 1.0])
+b = np.array([0.0, 1.0, 1.0])
+print 'wind', gain(n, start_num, a, b)
+
+
+
+# Outlook is rain
+n = 5.0
+start_num = 3.0
+
+# humidity
+a = np.array([2.0, 3.0])
+b = np.array([1.0, 2.0])
+print 'humidity', gain(n, start_num, a, b)
+
+
+# temp
+a = np.array([1.0, 3.0, 2.0])
+b = np.array([1.0, 2.0, 1.0])
+print 'temp', gain(n, start_num, a, b)
+
+
+# wind
+a = np.array([3.0, 2.0])
+b = np.array([3.0, 0.0])
+print 'wind', gain(n, start_num, a, b)
+
+
+
